d3bbf0e370c62b401c260a2678dc2057f8665d7e
Jaeseung <kimchi90216@gmail.com>
2024-07-01 21:36:47 +0900

    fix-commit

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
-// 구현 할 기능
-// 랜덤 번호 지정
-// 번호 입력 후 go 라는 버튼 누름
-// 만약에 유저가 번호를 맞추면 맞췄습니다
-// 번호 < 유저 번호 Down !
-// 번호 > 유저 번호 Up !
-// Reset 버튼을 누르면 게임이 리셋됨
-// 5번의 기회를 다쓰면 게임이 끝남 (더이상 추측 불가 , 버튼 사용 불가)
-// 유저가 1~100 범위 밖에 숫자를 입력하면 알려준다 (기회 안깍음)
-// 유저가 이미 입력한 숫자를 또 입력하면 알려준다 (기회 안깍음)
-
 let rn = 0
 let playbutton = document.getElementById("play-button")
 let userinput = document.getElementById("user-input")
 let resultarea = document.getElementById("result-area")
 let resetbutton = document.getElementById("reset-button")
-let chances = 5
+let chances = 3
 let gameover = false
 let chancearea = document.getElementById("chancearea")
 let history = []
+let answer = document.getElementById("answer-area")
 
 playbutton.addEventListener("click",play)
 resetbutton.addEventListener("click",reset)
 userinput.addEventListener("focus",function(){userinput.value=""})
+answer.addEventListener("click",showanswer)
 
 function pickrn() {
     rn = Math.floor(Math.random()*100)+1;
     console.log("정답",rn);
+}
+
+function showanswer() {
+    answer.textContent=`${rn} 이 정답입니다`
 }
 
 function play(){
@@ -42,17 +37,17 @@ function play(){
     
 
     chances --;
-    chancearea.textContent= `남은기회 : ${chances}번`
+    chancearea.textContent= `남은기회 : ${chances}번 | 입력했던 숫자들 : ${history.join(", ")}`
     console.log("chance",chances)
     if(uservalue<rn){
-        resultarea.textContent = "UP !!!"
+        resultarea.textContent = "업 !!!"
     }
     else if(uservalue>rn){
-        resultarea.textContent = "DOWN !!!"
+        resultarea.textContent = "다운 !!!"
     }
     else {
-        resultarea.textContent = "맞췄습니다 !!!"
-        gameover=true
+        resultarea.textContent = "맞췄습니다 축하합니다 !!!"
+        playbutton.disabled=true
     }
     history.push(uservalue)
     console.log(history)
@@ -60,18 +55,13 @@ function play(){
         gameover=true
     }
     if(gameover==true){
+        resultarea.textContent="기회를 모두 소진하였습니다 다시 하시려면 리셋 버튼을 눌려주세요"
         playbutton.disabled=true
     }
 }    
 
 function reset() {
-    userinput.value = ""
-    pickrn()
-    resultarea.textContent="결과값이 여기 나옵니다 !"
+    location.reload(true);
 }
 
-
-
 pickrn();
-
-
